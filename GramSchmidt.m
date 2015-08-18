f1f19e301495868930ff0936b7ee955c886791d9
parthshah31 <shahp@mit.edu>
2015-08-17 17:25:48 -0700

    Create GramSchmidt.m

---
+++ GramSchmidt.m
@@ -0,0 +1,22 @@
+function [Q,R,x] = GramSchmidt(M,b)
+%Implementation of Gram Schmidt process
+[m,n] = size(M);
+K = zeros(m,n);
+Q = zeros(m,n);
+K(1,:) = M(1,:);
+for i=2:n
+    K(i,:) = M(i,:);
+    j = i-1;
+    while j>0
+        K(i,:) = K(i,:) - ((transpose(K(j,:))*M(i,:))/(transpose(K(j,:))*M(j,:)))*K(j,:);
+        j = j-1;
+    end
+end
+
+for i=1:n
+    Q(i,:) = K(i,:)/norm(K(i,:));
+end
+R = transpose(Q)*M;
+y = A\transpose(Q);
+x = y*b;
+end
